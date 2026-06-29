42bcbb0be887e099ce759d0c55680c54d4e727e6
Mathew Merry <mat.merry@outlook.com>
2026-06-29 13:47:55 +0100

    Simple API request

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+PREFIX = "https://api.openf1.org/v1/"
+
+
+## Sends the API request and turns the returned value into JSON
+def request_api(url: str) -> requests.Response:
+    req = requests.get(url)
+    return req.json()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(request_api(PREFIX + "championship_drivers?session_key=latest"))
