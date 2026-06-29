37513add9f44ba28e06e022e900b7eb9047ad800
Mathew Merry <mat.merry@outlook.com>
2026-06-29 14:02:11 +0100

    Added an api request to get the driver's points

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ import requests
 PREFIX = "https://api.openf1.org/v1/"
 
 
-## Sends the API request and turns the returned value into JSON
-def request_api(url: str) -> requests.Response:
+# Sends the API request and turns the returned value into JSON
+def request_api(url: str):
     req = requests.get(url)
     return req.json()
 
 
+# Gets the points of the given driver
+def get_specific_driver_points(number: int) -> int:
+    req = request_api(f"{PREFIX}championship_drivers?session_key=latest&&driver_number={number}")
+    return req[0].get("points_current")
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(request_api(PREFIX + "championship_drivers?session_key=latest"))
+    print(get_specific_driver_points(12))
